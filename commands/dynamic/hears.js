fede92f069320b10777ddf9d2fe59d41bd5d0dbe
cdw-2014 <cdw2014@gmail.com>
2020-07-18 17:59:52 -0400

    BC-8: fixed dynamic word search triggering too often and moved Google sheet ID to env

--- commands/dynamic/hears.js
+++ commands/dynamic/hears.js
@@ -10,16 +10,17 @@ module.exports = {
   args: true,
   execute(client, message, args) {
     const content = message.content.toLowerCase();
+    const words = content.replace(/[^a-zA-Z0-9 ]/g, "").split(" ")
 
     if (message.author.id === "346865197530742786") {
       rambling.execute(client, message, args);
     }
 
-    if (content.includes("bruh") || content.includes(CUSTOM_WORD)) {
+    if (words.includes("bruh") || content.includes(CUSTOM_WORD)) {
       counter.execute(client, message, args);
-    } else if (content.includes("maze")) {
+    } else if (words.includes("maze")) {
       maze.execute(client, message, args);
-    } else if (content.includes("china")) {
+    } else if (words.includes("china")) {
       china.execute(client, message, args);
     }
   }
