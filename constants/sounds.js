f1e6356b50cd2f2245e69093b4d2f77825828cc4
cdw-2014 <cdw2014@gmail.com>
2020-12-31 23:04:03 -0500

    bruhsound multiple help pages

--- constants/sounds.js
+++ constants/sounds.js
@@ -1,177 +1,182 @@
 module.exports = [
-  {
-    name: "bruh",
-    value: "Bruh sound effect 2",
-    link: "https://www.youtube.com/watch?v=2ZIpFytCSVc"
-  },
-  {
-    name: "simp",
-    value: "Simp alert siren (loud)",
-    link: "https://www.youtube.com/watch?v=X2p1nqehXIM"
-  },
-  {
-    name: "thot",
-    value: "Thot depected (loud)",
-    link: "https://www.youtube.com/watch?v=NQlY7g-yamU"
-  },
-  {
-    name: "aiming",
-    value: "WHAT ARE YOU AIMING AT?! (loud)",
-    link: "https://www.youtube.com/watch?v=VoOxzT1ngeM"
-  },
-  {
-    name: "disappointment",
-    value: "My disappointment is immeasurable, and my day is ruined",
-    link: "https://www.youtube.com/watch?v=17KmNrG9pE4"
-  },
-  {
-    name: "ooohhh",
-    value: "OOOHHH!! (loud)",
-    link: "https://www.youtube.com/watch?v=82p43_udJg0"
-  },
-  {
-    name: "goodnight",
-    value: "TwoMad goodnight (long)",
-    link: "https://www.youtube.com/watch?v=5KZSzSSdUaA"
-  },
-  {
-    name: "failed",
-    value: "Mission Failed we'll get em next time",
-    link: "https://www.youtube.com/watch?v=dwLCjZVEtpE"
-  },
-  {
-    name: "goteem",
-    value: "HA, GOTEEM meme",
-    link: "https://www.youtube.com/watch?v=wnedkVrgFF0"
-  },
-  {
-    name: "ohshit",
-    value: "Joseph Joestar OH SHIT",
-    link: "https://www.youtube.com/watch?v=Ua20Hxx8xDE"
-  },
-  {
-    name: "holyshit",
-    value: "HOLY SHIT! Unreal Tournament Sound",
-    link: "https://www.youtube.com/watch?v=XNJ1RHUnX9s"
-  },
-  {
-    name: "bridge",
-    value: "PewDiePie saying the gamer word (long)",
-    link: "https://www.youtube.com/watch?v=QpaRFoCOV2Y&t=22s"
-  },
-  {
-    name: "poki",
-    value: "Pokimane saying the gamer word",
-    link: "https://www.youtube.com/watch?v=1yGLh2fSQ9Q"
-  },
-  {
-    name: "no",
-    value: "Tyler, The Creator saying 'N word uhhh no'",
-    link: "https://www.youtube.com/watch?v=nX_sZ-JuwI8"
-  },
-  {
-    name: "terry",
-    value: "Back up terry",
-    link: "https://www.youtube.com/watch?v=LJ-EC8TLvAo"
-  },
-  {
-    name: "stopit",
-    value: "Michael Jordan saying 'Stop it, get some help'",
-    link: "https://www.youtube.com/watch?v=9Deg7VrpHbM"
-  },
-  {
-    name: "suffer",
-    value: "Why are we here, just to suffer",
-    link: "https://www.youtube.com/watch?v=-QPcjVFHm2A"
-  },
-  {
-    name: "oof",
-    value: "Roblox OOF",
-    link: "https://www.youtube.com/watch?v=HoBa2SyvtpE"
-  },
-  {
-    name: "mustard",
-    value: "Spongebob movie mustard or ketchup glitch sound",
-    link: "https://www.youtube.com/watch?v=CCTnPEMs0SE"
-  },
-  {
-    name: "cum",
-    value: "You bet your sweet bippy I did",
-    link: "https://www.youtube.com/watch?v=joAV3JLSqQw"
-  },
-  {
-    name: "zombie",
-    value: "Zombie Bruh",
-    link: "https://www.youtube.com/watch?v=UtbtPdqzcas"
-  },
-  {
-    name: "vb",
-    value: "VB LONG NECK",
-    link: "https://www.youtube.com/watch?v=Ns15eHLDv1I"
-  },
-  {
-    name: "sister",
-    value: "mhm",
-    link: "https://www.youtube.com/watch?v=e5MYIZhQ5rY"
-  },
-  {
-    name: "gachihyper",
-    value: "AHHHHHHHHHHHHHHHHHHHHH",
-    link: "https://www.youtube.com/watch?v=I_VPb7IFXq4"
-  },
-  {
-    name: "laughing",
-    value: "do you see me laughin?",
-    link: "https://www.youtube.com/watch?v=5kNpMxjSJwE"
-  },
-  {
-    name: "showtime",
-    value: "its showtime",
-    link: "https://www.youtube.com/watch?v=bXTUJgpLDpQ"
-  },
-  {
-    name: "huh",
-    value: "huh",
-    link: "https://youtu.be/AZ2ppghJB4g"
-  },
-  {
-    name: "dang",
-    value: "Dang that look good",
-    link: "https://youtu.be/S1MI7qJeQUo"
-  },
-  {
-    name: "volume",
-    value: "Soulja Boy’s music is too loud",
-    link: "https://youtu.be/aAEAf60_iX8"
-  },
-  {
-    name: "2laugh",
-    value: "2Chainz Laugh",
-    link: "https://youtu.be/AczFdstkW54"
-  },
-  {
-    name: "urwelcome",
-    value: "You're Welcome",
-    link: "https://www.youtube.com/watch?v=2I-zV3mW3Cg"
-  },
-  {
-    name: "fuckyou",
-    value: "Van Darkholme says Fuck you",
-    link: "https://youtu.be/dNw4PwVYzSs"
-  },
-  {
-    name: "shungite",
-    value: "drdisrespect on shungite",
-    link: "https://youtu.be/Q3keX35f66Y"
-  },
-  {
-    name: "quake",
-    value: "Nuts are gonna start quaking",
-    link: "https://www.youtube.com/watch?v=9fryupLeBJw"
-  },
-  {
-    name: "raul",
-    value: "Dr Disrespect RAUL",
-    link: "https://youtu.be/F9ADj-9PcXs"
-  }
+	{
+		name  : 'bruh',
+		value : 'Bruh sound effect 2',
+		link  : 'https://www.youtube.com/watch?v=2ZIpFytCSVc'
+	},
+	{
+		name  : 'simp',
+		value : 'Simp alert siren (loud)',
+		link  : 'https://www.youtube.com/watch?v=X2p1nqehXIM'
+	},
+	{
+		name  : 'thot',
+		value : 'Thot depected (loud)',
+		link  : 'https://www.youtube.com/watch?v=NQlY7g-yamU'
+	},
+	{
+		name  : 'aiming',
+		value : 'WHAT ARE YOU AIMING AT?! (loud)',
+		link  : 'https://www.youtube.com/watch?v=VoOxzT1ngeM'
+	},
+	{
+		name  : 'disappointment',
+		value : 'My disappointment is immeasurable, and my day is ruined',
+		link  : 'https://www.youtube.com/watch?v=17KmNrG9pE4'
+	},
+	{
+		name  : 'ooohhh',
+		value : 'OOOHHH!! (loud)',
+		link  : 'https://www.youtube.com/watch?v=82p43_udJg0'
+	},
+	{
+		name  : 'goodnight',
+		value : 'TwoMad goodnight (long)',
+		link  : 'https://www.youtube.com/watch?v=7ALTB9Pj3DA'
+	},
+	{
+		name  : 'failed',
+		value : "Mission Failed we'll get em next time",
+		link  : 'https://www.youtube.com/watch?v=dwLCjZVEtpE'
+	},
+	{
+		name  : 'goteem',
+		value : 'HA, GOTEEM meme',
+		link  : 'https://www.youtube.com/watch?v=wnedkVrgFF0'
+	},
+	{
+		name  : 'ohshit',
+		value : 'Joseph Joestar OH SHIT',
+		link  : 'https://www.youtube.com/watch?v=Ua20Hxx8xDE'
+	},
+	{
+		name  : 'holyshit',
+		value : 'HOLY SHIT! Unreal Tournament Sound',
+		link  : 'https://www.youtube.com/watch?v=XNJ1RHUnX9s'
+	},
+	{
+		name  : 'bridge',
+		value : 'PewDiePie saying the gamer word (long)',
+		link  : 'https://www.youtube.com/watch?v=QpaRFoCOV2Y&t=22s'
+	},
+	{
+		name  : 'poki',
+		value : 'Pokimane saying the gamer word',
+		link  : 'https://www.youtube.com/watch?v=1yGLh2fSQ9Q'
+	},
+	{
+		name  : 'no',
+		value : "Tyler, The Creator saying 'N word uhhh no'",
+		link  : 'https://www.youtube.com/watch?v=nX_sZ-JuwI8'
+	},
+	{
+		name  : 'terry',
+		value : 'Back up terry',
+		link  : 'https://www.youtube.com/watch?v=LJ-EC8TLvAo'
+	},
+	{
+		name  : 'stopit',
+		value : "Michael Jordan saying 'Stop it, get some help'",
+		link  : 'https://www.youtube.com/watch?v=9Deg7VrpHbM'
+	},
+	{
+		name  : 'suffer',
+		value : 'Why are we here, just to suffer',
+		link  : 'https://www.youtube.com/watch?v=-QPcjVFHm2A'
+	},
+	{
+		name  : 'oof',
+		value : 'Roblox OOF',
+		link  : 'https://www.youtube.com/watch?v=HoBa2SyvtpE'
+	},
+	{
+		name  : 'mustard',
+		value : 'Spongebob movie mustard or ketchup glitch sound',
+		link  : 'https://www.youtube.com/watch?v=CCTnPEMs0SE'
+	},
+	{
+		name  : 'cum',
+		value : 'You bet your sweet bippy I did',
+		link  : 'https://www.youtube.com/watch?v=joAV3JLSqQw'
+	},
+	{
+		name  : 'zombie',
+		value : 'Zombie Bruh',
+		link  : 'https://www.youtube.com/watch?v=UtbtPdqzcas'
+	},
+	{
+		name  : 'vb',
+		value : 'VB LONG NECK',
+		link  : 'https://www.youtube.com/watch?v=Ns15eHLDv1I'
+	},
+	{
+		name  : 'sister',
+		value : 'mhm',
+		link  : 'https://www.youtube.com/watch?v=e5MYIZhQ5rY'
+	},
+	{
+		name  : 'gachihyper',
+		value : 'AHHHHHHHHHHHHHHHHHHHHH',
+		link  : 'https://www.youtube.com/watch?v=I_VPb7IFXq4'
+	},
+	{
+		name  : 'laughing',
+		value : 'do you see me laughin?',
+		link  : 'https://www.youtube.com/watch?v=5kNpMxjSJwE'
+	},
+	{
+		name  : 'showtime',
+		value : 'its showtime',
+		link  : 'https://www.youtube.com/watch?v=bXTUJgpLDpQ'
+	},
+	{
+		name  : 'huh',
+		value : 'huh',
+		link  : 'https://youtu.be/AZ2ppghJB4g'
+	},
+	{
+		name  : 'dang',
+		value : 'Dang that look good',
+		link  : 'https://youtu.be/S1MI7qJeQUo'
+	},
+	{
+		name  : 'volume',
+		value : 'Soulja Boy’s music is too loud',
+		link  : 'https://youtu.be/aAEAf60_iX8'
+	},
+	{
+		name  : '2laugh',
+		value : '2Chainz Laugh',
+		link  : 'https://youtu.be/AczFdstkW54'
+	},
+	{
+		name  : 'urwelcome',
+		value : "You're Welcome",
+		link  : 'https://www.youtube.com/watch?v=2I-zV3mW3Cg'
+	},
+	{
+		name  : 'fuckyou',
+		value : 'Van Darkholme says Fuck you',
+		link  : 'https://youtu.be/dNw4PwVYzSs'
+	},
+	{
+		name  : 'shungite',
+		value : 'drdisrespect on shungite',
+		link  : 'https://youtu.be/Q3keX35f66Y'
+	},
+	{
+		name  : 'quake',
+		value : 'Nuts are gonna start quaking',
+		link  : 'https://www.youtube.com/watch?v=9fryupLeBJw'
+	},
+	{
+		name  : 'raul',
+		value : 'Dr Disrespect RAUL',
+		link  : 'https://youtu.be/F9ADj-9PcXs'
+	},
+	{
+		name  : 'wocky',
+		value : 'Wocky Slush',
+		link  : 'https://www.youtube.com/watch?v=KknzBT95GqQ'
+	}
 ];
