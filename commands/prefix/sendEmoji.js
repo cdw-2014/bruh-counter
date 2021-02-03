62b2db12197fd58cb4b89334416c4a466efb1881
cdw-2014 <cdw2014@gmail.com>
2021-02-03 01:21:09 -0500

    added send emoji command

---
+++ commands/prefix/sendEmoji.js
@@ -0,0 +1,14 @@
+const emojis = require('../../constants/emojis');
+
+module.exports = {
+	name        : '!emoji',
+	description : 'Use GIF emojis and external emojis',
+	args        : true,
+	execute(client, message, args) {
+		emojis.forEach((emoji) => {
+			if (args[0] === emoji.name) {
+				message.channel.send(`<:${emoji.name}:${emoji.id}>`);
+			}
+		});
+	}
+};
